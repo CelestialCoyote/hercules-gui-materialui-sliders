7a99e6ebe12677cbe623b6906554b17c811f5676
Paul Stearns <celestialcoyote@gmail.com>
2022-09-27 17:18:33 -0600

    Added backend.

---
+++ frontend/src/userConfig/backendIP.js
@@ -0,0 +1,6 @@
+//const backend = {"IP": "http://192.168.1.37:3042"}; // InfiniteImprobabilityDrive
+const backend = {"IP": "http://192.168.1.147:3009"}; // PanGalacticGargleBlaster
+
+
+
+export default backend;
