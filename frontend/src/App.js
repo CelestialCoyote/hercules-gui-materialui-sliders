dee89c267cadaa2b4dee5e283811a3fd08ae0222
Paul Stearns <celestialcoyote@gmail.com>
2022-09-26 18:52:30 -0600

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Hercules from "./components/Hercules/Hercules";
+
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <Hercules />
+        </div>
+    );
+}
+
+
+export default App;
